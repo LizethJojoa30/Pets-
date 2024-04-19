7a4ac859beeb32808a3e055333debb5057fcb7b3
Lizeth <Lizethjojoa3@gmail>
2024-04-19 11:53:08 -0500

    envio archivo

---
+++ src/Backend/singup.php
@@ -0,0 +1,22 @@
+<?php
+    include('../../config/database.php');
+
+    $fullname = $_POST['fname'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $enc_pass = md5($password);
+
+    $sql = "
+        INSERT INTO users (fullname, email, password) 
+            VALUES ('$fullname', '$email','$enc_pass')
+    ";
+
+    $ans = pg_query($conn,$sql);
+    if ($ans){
+        echo "User has been created successfully";
+    }else{
+        echo "Error: " . pg_last_error();
+    }
+
+    
+?>
