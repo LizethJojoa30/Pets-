9aa427ef64f03d6a27d2948a1d8d56fa046d75a2
Lizeth <Lizethjojoa3@gmail>
2024-05-03 12:37:21 -0500

    envio archivo corte 2

--- src/Backend/singup.php
+++ src/Backend/singup.php
@@ -6,6 +6,14 @@
     $password = $_POST['password'];
     $enc_pass = md5($password);
 
+    $sql_validate_email = "SELECT * FROM users WHERE email ='$email'";
+    $result = pg_query($conn, $sql_validate_email);
+    $total= pg_num_rows($result);
+
+    if($total>0){
+        echo "<script>alert('Email has been registered')</script>";
+        header("refresh:0; url=../Home.php");
+    }else{
     $sql = "
         INSERT INTO users (fullname, email, password) 
             VALUES ('$fullname', '$email','$enc_pass')
@@ -13,10 +21,12 @@
 
     $ans = pg_query($conn,$sql);
     if ($ans){
-        echo "User has been created successfully";
+        echo "<script>alert('User has been registered')</script>";
+        header("refresh:0; url=../Home.php");
     }else{
+       
         echo "Error: " . pg_last_error();
     }
-
-    
+    }
+pg_close($conn)
 ?>
